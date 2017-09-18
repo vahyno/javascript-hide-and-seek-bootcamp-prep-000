b83a847a7fb6f1a857c8d0299d427bb67700db44
M V <vahyno+learn@gmail.com>
2017-09-18 04:21:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,46 @@ function deepestChild(){
 }
 
 function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  const firstList = rankedLists[0];
+  const secondList = rankedLists[1];
 
+  let children = firstList.children
+  let start = 1
+
+  for (let i = 0, l = children.length; i < l; i++) {
+    (parseInt(children[i].innerHTML +n))
+  }
+
+  
+  while (current) {
+    if (criteriaFn(current)) {
+      return current
+    }
+    if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i])
+      }
+    }
+    current = next.shift()
+  }
+  return null
 }
+
+/*const rankedLists = document.querySelectorAll('.ranked-list')
+const firstList = rankedLists[0]
+const secondList = rankedLists[1]
+
+let children = firstList.children
+let start = 1
+for (let i = 0, l = children.length; i < l; i++) {
+  expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
+}
+
+children = secondList.children
+start = 12
+
+for (let i = 0, l = children.length; i < l; i++) {
+  expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3)
+*/
+
