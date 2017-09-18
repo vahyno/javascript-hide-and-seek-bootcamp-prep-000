13469cef36fb088c2e750ff602ae479b0dd83bed
M V <vahyno+learn@gmail.com>
2017-09-18 03:07:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function nestedTarget(){
   return document.querySelector('#nested .target');
 }
 
+function increaseRankBy(n){
+  
+}
+
 function deepestChild(){
   document.querySelector('#grand-node div div div div');
 }
