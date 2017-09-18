5878bc3da85a81b3e463a8dc028d6fec196ef062
M V <vahyno+learn@gmail.com>
2017-09-18 04:54:45 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,32 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
 
+/*function deepestChild(){
+  return document.querySelector('#grand-node div div div div');
+}*/
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j =0; j < children.length; j++){
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
 }
--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
-/*describe('index', () => {
+describe('index', () => {
   describe('getFirstSelector(selector)', () => {
     it('returns the first element that matches the selector', () => {
       expect(getFirstSelector('div').id).toEqual('nested')
@@ -42,4 +42,3 @@
     })
   })
 })
-*/
